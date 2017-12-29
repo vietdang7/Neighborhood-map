f0ba255f50e33bc06a5b8b25522e11dd62bc7085
Viet Dang Quoc <thedangs7@gmail.com>
2017-12-29 23:23:51 +0200

    Edit zoom level in app.js and add error message to #map div

--- js/app.js
+++ js/app.js
@@ -47,7 +47,7 @@ function ViewModel() {
   // Setup Helsinki Map
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 60.169856, lng: 24.938379},
-    zoom: 17
+    zoom: 15
   });
 }
 
