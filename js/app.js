a0c072b48353a77803629f61696f60bd1883d5d0
Viet Dang Quoc <thedangs7@gmail.com>
2017-12-29 21:59:41 +0200

    Increase zoom level to 20

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
      function initMap() {
        map = new google.maps.Map(document.getElementById('map'), {
          center: {lat: 60.169856, lng: 24.938379},
-         zoom: 14
+         zoom: 20
        });
-       
+
      }
