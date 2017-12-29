897743ec59fde8816a331f8e41b2f77478c2878c
Viet Dang Quoc <thedangs7@gmail.com>
2017-12-29 22:01:49 +0200

    Add "place" variable

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 // Create a map variable
-     var map;
-     // Function to initialize the map within the map div
-     function initMap() {
-       map = new google.maps.Map(document.getElementById('map'), {
-         center: {lat: 60.169856, lng: 24.938379},
-         zoom: 20
-       });
+var map;
 
-     }
+// Create a variable for Place
+var place;
+
+// Function to initialize the map within the map div
+function initMap() {
+ map = new google.maps.Map(document.getElementById('map'), {
+   center: {lat: 60.169856, lng: 24.938379},
+   zoom: 20
+ });
+
+}
